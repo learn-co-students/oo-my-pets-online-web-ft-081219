880d1bc8321c684a728ad8e886abc46c43ddf9ba
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-31 01:44:34 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,60 @@
+require 'pry'
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :pets
+  
+  @@all = []
+  @@pets = { :dogs => [], :cats => [] }
+  def initialize(name)
+    @name = name 
+    @species = "human"
+     @@all << self
+  # binding.pry
+  end
+  
+  def say_species 
+    "I am a human."
+  end
+
+  def self.all 
+    @@all
+  end
+  
+  def self.count 
+   @@all.length
+  end
+  
+  def self.reset_all
+     @@all.clear
+  end
+  def pets 
+    @@pets
+  end
+  def cats 
+    Cat.all.select{|cat| cat.owner == self}
+  end
+  
+  def dogs 
+    Dog.all.select{|dog| dog.owner == self}
+  end
+  
+  def buy_cat(cat_name)
+    @@pets[:cats] << Cat.new(cat_name,self)
+  end
+  
+  def buy_dog(dog_name)
+    @@pets[:dogs] << Dog.new(dog_name,self)
+  end
+  
+  def walk_dogs
+    @@pets.map do |pet, array|
+      if pet == :dogs
+        array.collect do |dog|
+          dog.mood = "happy"
+        end
+        # binding.pry
+      end
+    end
+  end
+  
 end
